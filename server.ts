455631b63fa129d7cf15215984efd62aef313ff5
5war00p <57614947+5war00p@users.noreply.github.com>
2021-11-02 00:38:43 +0530

    new registration & packages
    new registrations done on server.ts
    new packages installed

--- server.ts
+++ server.ts
@@ -1,23 +1,33 @@
 require("dotenv").config();
 
+// Imports from packages
 import Fastify from "fastify";
-import "./db/db-connector";
-import { commonroutes } from "./routes/common-routes";
 
-const API_PORT = process.env.API_PORT || "9040";
+// Local imports
+import db from "./db/db-connector";
+import { commonRoutes } from "./routes/common-routes";
+import { userRoutes } from "./routes/user-routes";
+import { shortcutRoutes } from "./routes/shortcut-routes";
+
+const API_PORT = process.env.API_PORT || 8070;
 const API_BIND_ADDR = process.env.API_BIND_ADDR || "0.0.0.0";
 
-const fastify = Fastify({ logger: true });
+const app = Fastify({ logger: true });
 
-// registering common routes
-fastify.register(commonroutes);
+// registering
+app.register(commonRoutes, { prefix: "/common" });
+app.register(userRoutes, { prefix: "/user" });
+app.register(shortcutRoutes, { prefix: "/user/shortcut" });
 
-fastify.listen(API_PORT, API_BIND_ADDR, (err, address) => {
+app.listen(API_PORT, API_BIND_ADDR, (err, address) => {
   if (err) {
-    fastify.log.error(err);
+    app.log.error(err);
     process.exit(1);
   }
+
+  // Connceting to DB
+  db.connectDB();
   console.info(`Server listening on ${address}`);
 });
 
-export { fastify };
+export default app;
