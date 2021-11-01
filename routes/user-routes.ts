3e19981f1f77f38f131df070e1648c0a6292e3fb
5war00p <57614947+5war00p@users.noreply.github.com>
2021-11-02 00:37:27 +0530

    User routes added
    A profile displaying route added

---
+++ routes/user-routes.ts
@@ -0,0 +1,25 @@
+import models from "../db/db-connector";
+import funcs from "../utils/funcs";
+import jwtManager from "../utils/jwt-manager";
+
+export async function userRoutes(fastify: any, options: any) {
+  // Profile route
+  fastify.get(
+    "/profile",
+    { preHandler: jwtManager("access_token") },
+    async (req: any, res: any, next: any) => {
+      const _id = req.jwt_data._id;
+      if (!models.mongoose.Types.ObjectId.isValid(_id))
+        return funcs.sendError(res, "Invalid UserID!", 403);
+
+      models.User.findById(_id)
+        .select({ email: 1, name: 1, shortcuts: 1 })
+        .then((user: any) => {
+          if (!user) throw { message: "User not found", code: 404 };
+
+          return funcs.sendSuccess(res, user);
+        })
+        .catch(next);
+    }
+  );
+}
