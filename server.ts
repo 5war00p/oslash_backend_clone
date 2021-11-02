2e4a886a49c144a7adbfa5318a331b37ac29b364
5war00p <57614947+5war00p@users.noreply.github.com>
2021-11-02 17:10:03 +0530

    updated server.ts and added documentation

--- server.ts
+++ server.ts
@@ -1,13 +1,13 @@
-require("dotenv").config();
-
 // Imports from packages
 import Fastify from "fastify";
+require("dotenv").config(); //.env configuration
 
 // Local imports
-import db from "./db/db-connector";
-import { commonRoutes } from "./routes/common-routes";
+import { authRoutes } from "./routes/auth-routes";
 import { userRoutes } from "./routes/user-routes";
+import { commonRoutes } from "./routes/common-routes";
 import { shortcutRoutes } from "./routes/shortcut-routes";
+import db from "./db/db-connector";
 
 const API_PORT = process.env.API_PORT || 8070;
 const API_BIND_ADDR = process.env.API_BIND_ADDR || "0.0.0.0";
@@ -15,18 +15,18 @@ const API_BIND_ADDR = process.env.API_BIND_ADDR || "0.0.0.0";
 const app = Fastify({ logger: true });
 
 // registering
-app.register(commonRoutes, { prefix: "/common" });
 app.register(userRoutes, { prefix: "/user" });
+app.register(authRoutes, { prefix: "/auth" });
+app.register(commonRoutes, { prefix: "/common" });
 app.register(shortcutRoutes, { prefix: "/user/shortcut" });
 
-app.listen(API_PORT, API_BIND_ADDR, (err, address) => {
+app.listen(API_PORT, API_BIND_ADDR, async (err, address) => {
   if (err) {
     app.log.error(err);
-    process.exit(1);
+    // process.exit(1);
   }
 
-  // Connceting to DB
-  db.connectDB();
+  await db.connectDB();
   console.info(`Server listening on ${address}`);
 });
 
